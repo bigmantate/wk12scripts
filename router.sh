c2f3729341efcbbf77ae1a9402553662714e1768
bigmantate <35516914+bigmantate@users.noreply.github.com>
2018-04-03 23:18:23 -0500

    Update router.sh

--- router.sh
+++ router.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
+3#!/bin/bash
 #Routing script
 
 echo "IP routing is the process by which a host with multiple network"
 echo "connections decides where to deliver IP datagrams it has recieved"
 
-sleep 1
+sleep 2
 
 echo "Using the ifconfig command we will look at our network info"
 
@@ -23,18 +23,18 @@ echo "In Linux you can see this table with the following command"
 
 cat /proc/net/route
 
-sleep 1
+sleep 3
 
 echo "Another command netstat will also give you similar information"
 
 netstat -r
 
-sleep 1
+sleep 3
 
 echo "One more command to provide you with another table layout"
 
 /sbin/route -n
 
-sleep 1
+sleep 2
 
 echo ""
